7cb89a49b3de6afa6d0a20beaf92b461d784231b
wael-zuaiter <wa95el@hotmail.com>
2020-07-13 14:52:48 +0300

    Init Commit

---
+++ src/yourdata.js
@@ -0,0 +1,86 @@
+export default
+    {
+        //(Please Do Not Remove The comma(,) after every variable)
+        //Change The Website Template
+        name: 'Wael Zoaiter.',
+        headerTagline: [//Line 1 For Header
+            'Hi 👋 You found me! I am Wael Zoaiter.',
+            //Line 2 For Header
+            'Frontend / Mobile Developer ',
+            //Line 3 For Header
+            'from Syria'
+        ],
+        //Contact Email
+        contactEmail: 'wael.zoaiter@gmail.com',
+        // Add Your About Text Here
+        abouttext: "I’m a Web and Mobile application developer with industry experience building web and mobile applications. I specialize in JavaScript and have professional experience working with React js, React Native and Redux. I also have experience working with Node js for building RestFull APIs.",
+        aboutImage: 'https://cz8k6g.dm.files.1drv.com/y4mt59x1gSu-8PWZP1QBucV9BVYFKHYi__XunZwu7m1qNd2ALVnr3-feIvPzgUa76lpenq96IHwioUx50waPXYIXWvgl6f57XgyZ6WIuubXTUKXwB8gswmYqCKxskkIcf0CF6kNJuPpKIIlgmnb6efnIRoH4ZmSDPQLq_XXvvgjLf7HUmptI9bEs2KVjM5iaHAn6DrYfAUCT74xAY8EoVli5g?width=2061&height=2033&cropmode=none',
+        //Change This To Hide The Image of About Section (True Or False)
+        ShowAboutImage: false,//true or false (Change Here)
+        // Change Projects Here 
+        projects: [
+            {
+                id: 1,//DO NOT CHANGE THIS (Please)😅
+                title: 'Forsa.sy Website', //Project Title - Add Your Project Title Here
+                service: 'Web Application', // Add Your Service Type Here
+                //Project Image - Add Your Project Image Here
+                imageSrc: "https://cp92va.dm.files.1drv.com/y4m1A4Lq4Xs0kyS6EwPlt_N24oSbsS24Ga0NAf14yGGfQMagnI0lSWzIuTofjF58AX_R2rT7rEmAiJk3OQbgCQ4-iNw-5LVQewLxK8r55cQeo_1sgkqAlsIXd2bidhv-lHbQThMbD9xtCnuHNY4Xv09c3mGLFjbPcuwQCr7wtur2C2oEDJpnyBsLb3FbwjXfsobQVMiyE7dVI_RzyqUkjky9A?width=1334&height=805&cropmode=none",
+                //Project URL - Add Your Project Url Here
+                url: 'http://forsa.sy/'
+            },
+            {
+                id: 2,//DO NOT CHANGE THIS (Please)😅
+                title: 'iEducate App',
+                service: 'Mobile Application',
+                imageSrc: "https://dp92va.dm.files.1drv.com/y4mO5qzSxHZOEAzn_vNJsszxS1BtZ713hb6vAQYIE3JOLtliZ2HU8_qJWjpxd_dTfO6jIezD3Z_Kt2Ij9zD7gh3Dp2UiyVXCj-3FJn1C-5uPB8CRg2apVR5ORLcSHBxp6e6xIHD9qGnswf44AOq2szWAzcL2bys4NrVMFsx1ptJ5tUg0LhjTpOMzeDtYVGrqZuDgTGNPMJFiv1ZstiGas5QJA?width=640&height=1280&cropmode=none",
+                url: 'https://ieducate.netlify.com/'
+            },
+            {
+                id: 3,//DO NOT CHANGE THIS (Please)😅
+                title: 'iEducate Admin Dashboard',
+                service: 'Web Application',
+                imageSrc: "https://d592va.dm.files.1drv.com/y4mXswpQkLUQcTM90z2rAre6hOetimx0Y_10L5AEufjRT7qw6hsLRpKitCBtTJsk01xJUiwEnanBB4OIL0UzZHxVZXM294wInUs4WEYCQ5JppfhyEEoEQAf9JuOZyYhQbIvVfUYxebm9myr0j3ET3HpfvlEBrkon6ZHEDzLWCVLEicpQ2sjMWgNah4BoDUSFe2_aoPZ9s33eM3T1C5rwLTd-Q?width=1706&height=811&cropmode=none",
+                url: 'https://ieducate.netlify.com/'
+            },
+            {
+                id: 4,//DO NOT CHANGE THIS (Please)😅
+                title: 'Css library',
+                service: 'UI/UX Development',
+                imageSrc: "https://cz92va.dm.files.1drv.com/y4mBDJoQ19P-HT_vj47uVXvSuTKbpIs0CHjQbdb7Al6XOG_j0bjpQm9RXhFi1XMA33yKnp4RylOF_KU0due3vcOssLCazJGlahYxV-iC-mT7jL03XPz_Cr845_eyIYQS9lQP55NdkGP0Gz5Lx2r_4KOxgevz184XweGrnzlLoCARujGDpwmAiUSZYgHN6qPVH7AaVs_xfnf6_AwcT4KpMO0Bg?width=1708&height=801&cropmode=none",
+                url: 'https://webinjaz.herokuapp.com'
+            },
+            {
+                id: 5,//DO NOT CHANGE THIS (Please)😅
+                title: 'Daily World',
+                service: 'React.js SPA App',
+                imageSrc: "https://cj92va.dm.files.1drv.com/y4mmLJ-7RlJNtwvaadWyMCzpYotAtMLCmSF6PqI8uRVC6ZcnPWjNrt1TTT71ixu3C6w9LLrM8WJM9J4Z1XftCS6djgDPh9aD1-xvWh3QmftAmC0d6SxQD_bbH0UKXJkQqxSnrWK5x2_nLvu09jtdE6nwfkmskiX6rPSIL-57MXi54vl-RQpScYfQNEmAaoTWWSxQEtVOdzCOzLc3V4BJmFO7A?width=1708&height=741&cropmode=none",
+                url: 'https://wael-zoaiter.github.io/daily-world/'
+            },
+            {
+                id: 6,//DO NOT CHANGE THIS (Please)😅
+                title: 'Chat Rooms',
+                service: 'Backend Web Development',
+                imageSrc: "https://c592va.dm.files.1drv.com/y4mFRl8QL4eBFLkrFqQlFHCRbNXhYEZd6eVL4dpN5gP73JMJHeyE0lczFPVNe40mLp2j5gY3SoFXm67A-2x-kXRrKfSxmLCkvtRcY5-QEc_4vmDAeKVzq0QoJB5AL-xldrztB8sMxtSZP_ySELlh3Ujt85r7i_M-MoVrLIv32S9CGxVQYdQmggXMMNrtyPDy-KTq5qgC8f3GhI7wg0o5pq7GA?width=1706&height=807&cropmode=none",
+                url: 'https://chat-w-app.herokuapp.com'
+            }
+        ],
+        social: [
+            // Add Or Remove The Link Accordingly
+            {
+                name: 'Github',
+                url: 'https://github.com/wael-zoaiter'
+            },
+            // {
+            //     name: 'Behance',
+            //     url: 'https://www.behance.net/chetanverma'
+            // },
+            // {
+            //     name: 'Dribbble',
+            //     url: 'https://dribbble.com/chetanverma'
+            // },
+            // {
+            //     name: 'Instagram',
+            //     url: 'https://www.instagram.com/cv.uidesign/'
+            // }
+        ]
+    }
